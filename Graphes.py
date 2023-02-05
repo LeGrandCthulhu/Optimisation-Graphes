790d47697f8fee4e511e39f8e8005fa6411771a4
LeGrandCthulhu <pierre.frebet@gmail.com>
2023-02-05 15:49:39 +0100

    temp car problème de Vs

--- Graphes.py
+++ Graphes.py
@@ -12,7 +12,7 @@ root.config(background='black')
 para = tk.Frame(root, bg="black")
 texte_explications = tk.Label(para, text="Rentrez le nombre de noeuds dans le graphes", bg="black", font=("Arial", 15), fg="white")
 entree = tk.Entry(para, bg="white", font=("Arial", 15))
-validation = tk.Button(para, text="Valider", bg="grey", fg="white", font=("Arial", 15))
+validation = tk.Button(para, text="Valider", bg="grey", fg="white", font=("Arial", 15), command=lambda: gf)
 para.grid(row=0, column=1, sticky=tk.N)
 texte_explications.grid(row=0, column=0)
 entree.grid(row=1, column=0)
@@ -21,6 +21,9 @@ validation.grid(row=2, column=0)
 
 #Création du canvas des graphes
 canvas = tk.Canvas(root, bg="black", height=1013, width=1500, highlightthickness=0)
+
+#Création du graphe
+
 
 
 canvas.grid(row=0, column=0)
